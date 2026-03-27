2d0bc6d610babddfdfc7bee74c15c220e8e3d831
praveenkumar344 <praveenthiru344@gmail.com>
2026-03-27 10:16:50 +0530

    first commit

---
+++ pages/homePage.js
@@ -0,0 +1,31 @@
+class HomePage{
+constructor(page){
+    this.page=page;
+    this.searchBox=page.locator('.input-search');
+     this.email=page.locator('[type="email"]');
+        this.password=page.locator('[type="password"]');
+        this.alwaysSignIn=page.locator('#true');
+        this.closePopUp=page.locator(".onetrust-close-btn-handler.onetrust-close-btn-ui.banner-close-button.ot-close-icon")
+        this.signInButton=page.locator('button:has-text("Sign In")');
+    
+}
+
+async goto() {
+    await this.page.goto(process.env.Base_URL);
+  }
+
+async search(item){
+    await this.searchBox.fill(item);
+    await this.page.keyboard.press("Enter");
+}
+ async signIn(email,password){
+    await this.email.fill(email);
+    await this.password.fill(password);
+    await this.alwaysSignIn.click();
+    await this.closePopUp.click();
+    await this.signInButton.click();
+  }
+
+}
+
+export default HomePage;
---
+++ tests/example.spec.js
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import CreateAccount  from '../pages/createAccount';
+import SignIn from '../pages/signIn';
+import HomePage from '../pages/homePage';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+test.skip('successful account creation', async ({ page }) => {
+  const caPage = new CreateAccount(page);
+  await caPage.goto();
+  await caPage.createAcc('John', 'wick',process.env.USER_EMAIL,process.env.USER_PASSWORD,'August','08','4564564567');
+  // await page.waitForTimeout(10000);
+  await expect(page.locator('.p-message-wrapper')).toContainText('Congrats! You created a Macy’s account & added your Star Rewards membership.');
+});
+
+test('successful Login', async ({ page }) => {
+  const siPage = new SignIn(page);
+  await siPage.goto();
+  await siPage.signIn(process.env.USER_EMAIL,process.env.USER_PASSWORD);
+  await page.waitForTimeout(5000);
+  // await page.locator('i.icon-28x28.profile').click();
+  await expect(page.locator('span:has-text("Hi, John")')).toHaveText('Hi, John');
+
+});
+
+test('search bag ',async({page})=>{
+  const hmPage=new HomePage(page);
+  await hmPage.goto();
+   await hmPage.signIn(process.env.USER_EMAIL,process.env.USER_PASSWORD);
+  await hmPage.search("bag");
+  // test.true();
+  expect(true).toBeTruthy();
+
+})
